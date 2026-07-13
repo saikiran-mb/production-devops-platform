569431fde501834144b0ecf4218fc2ee390903b0
Sai Kiran M.B <saikirank1206@gmail.com>
2026-07-14 00:36:55 +0530

    Add backend pom.xml and Jenkins CI configuration

---
+++ backend/src/main/java/com/equence/deploytracker/DeploytrackerApplication.java
@@ -0,0 +1,13 @@
+package com.equence.deploytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploytrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploytrackerApplication.class, args);
+    }
+
+}
